7a5630b03bf9cc60e582c678b1b1fdb505b50cfe
Clara Le Roux <clara.mg.leroux@gmail.com>
2019-11-14 15:17:38 +0100

    woup woup

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,5 @@
 class DosesController < ApplicationController
-  before_action :set_dose, only: [:show ]
+  before_action :set_dose, only: [:show, :destroy ]
 
   def new
 
@@ -9,23 +9,24 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-    @dose.save
-    redirect_to cocktail_path(@cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
   end
 
   def destroy
     @dose.destroy
-
-    redirect_to doses_path
+    #redirect_to doses_path
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 
   def set_dose
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   resources :cocktails do
     resources :doses, only: [ :index, :new, :create ]
   end
-   resources :doses, only: [ :show, :edit, :update, :destroy ]
- end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doses, only: [:destroy ]
+end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
