9e4108ffad60a83632dbc005469d13f5c73d0b6e
Clara Le Roux <clara.mg.leroux@gmail.com>
2019-11-14 14:48:11 +0100

    first draft

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:show ]
+
+  def new
+
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose.destroy
+
+    redirect_to doses_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+end
+
+
+
---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true, allow_blank: false
+  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :ingredient_id, presence: true, allow_blank: false
+
+end
